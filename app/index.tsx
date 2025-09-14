00dadce659963216124ad7e97f68a74e96b0e07a
yldrmahmet <ay689089@gmail.com>
2025-09-14 15:33:03 +0300

    complete nativewind setup and test in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -3,14 +3,10 @@ import "./global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>ahmet</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
