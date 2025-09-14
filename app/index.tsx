f6508681e629f7e3eb9e6f99b645ac98173cf32e
yldrmahmet <ay689089@gmail.com>
2025-09-14 16:25:38 +0300

    extend color palette in Tailwind configuration

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,7 @@ import "./global.css";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-bold text-dark-200">Welcome!</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "151312",
+        accent: "AB8BFF",
+        light: {
+          100: "#d6c6ff",
+          200: "#a8b5db",
+          300: "#9ca4ab",
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+      },
+    },
   },
   plugins: [],
 };
